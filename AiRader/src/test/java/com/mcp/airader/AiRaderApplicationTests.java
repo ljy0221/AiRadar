a74bd3fee4e0942b8ad0e0f82433467aff499ffb
Swimming_Yang <ysw1mst@gmail.com>
2026-03-03 14:33:42 +0900

    FEAT: init proj
    add Gradle dependencies

---
+++ AiRader/src/test/java/com/mcp/airader/AiRaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mcp.airader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiRaderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
