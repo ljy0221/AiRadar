4dd83bfc3cd74c31a3d17509349b28d919313e62
woduddl1000 <woduddl1000@gmail.com>
2026-03-16 14:54:22 +0900

    fix: link trigger when manual laod

--- AIRadar/airflow/dags/silver_refinement_dag.py
+++ AIRadar/airflow/dags/silver_refinement_dag.py
@@ -5,6 +5,9 @@ from airflow.operators.bash import BashOperator
 from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
 from airflow.sensors.external_task import ExternalTaskSensor
 from airflow.utils.task_group import TaskGroup
+from airflow.utils.session import provide_session
+from airflow.models import DagRun
+from airflow.utils.state import State
 
 default_args = {
     'owner': 'ai-radar',
@@ -34,11 +37,29 @@ with DAG(
     tags=['silver', 'batch'],
 ) as dag:
 
+    def get_latest_bronze_execution_date(execution_date, **kwargs):
+        """가장 최근 성공한 bronze_kafka_ingestion run의 execution_date 반환"""
+        from airflow.utils.session import create_session
+        with create_session() as session:
+            last_run = (
+                session.query(DagRun)
+                .filter(
+                    DagRun.dag_id == 'bronze_kafka_ingestion',
+                    DagRun.state == State.SUCCESS,
+                )
+                .order_by(DagRun.execution_date.desc())
+                .first()
+            )
+        if last_run:
+            return last_run.execution_date
+        return execution_date
+
     # Bronze 적재 완료 후 Silver 정제 실행
     wait_for_bronze = ExternalTaskSensor(
         task_id='wait_for_bronze_ingestion',
         external_dag_id='bronze_kafka_ingestion',
         external_task_id=None,   # DAG 전체 완료 대기
+        execution_date_fn=get_latest_bronze_execution_date,
         timeout=3600,
         poke_interval=60,
         mode='reschedule',       # slot을 점유하지 않고 대기
